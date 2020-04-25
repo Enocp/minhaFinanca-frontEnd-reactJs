e7bc9608451d918e4fc63acaebb16f9312ce380c
Enocp <pierre.enoc@gmail.com>
2020-04-25 17:45:24 -0300

    bricando com compomente

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 
-function App() {
-  return (
-    <div >
-      Hello paris!!
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome: ''
+  }
+
+  render() {
+    return (
+      <div>
+        <label> Nome:</label>
+        <input type="texte" value={this.state.nome}
+          onChange={(e) => this.setState({ nome: e.target.value })} />
+
+       O nome digitado foi :{this.state.nome}
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
