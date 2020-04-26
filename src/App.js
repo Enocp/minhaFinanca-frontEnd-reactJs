185b615f9c18297b4151f46a09e5591ab4001fda
Enocp <pierre.enoc@gmail.com>
2020-04-26 01:19:37 -0300

    criando components Login card

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
+import 'bootswatch/dist/flatly/bootstrap.css'
+import Login from './views/login'
 //import logo from './logo.svg';
 //import './App.css';
 
 class App extends React.Component {
 
-  state = {
-    numero1: 'null',
-    nnumero2: 'null',
-    resultado: 'null'
-  }
+
 
   render() {
     return (
       <div>
-        <label> primeiro Numero:</label>
-        <input type="texte" value={this.state.numero1}
-          onChange={(e) => this.setState({ numero1: e.target.value })} />
-        <br />
-        <label> segundo numero:</label>
-        <input type="texte" value={this.state.numero2}
-          onChange={(e) => this.setState({ numero2: e.target.value })} />
-        <br />
-        <button
-          onClick={() => this.setState({ resultado: parseInt(this.state.numero1) + parseInt(this.state.numero2) })} >
-          Somar</button>
-        <br />
-        O resultado è :{this.state.resultado}
-
+        <Login />
       </div>
     )
   }
