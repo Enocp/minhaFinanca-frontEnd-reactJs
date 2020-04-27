7eeb4e1a2c0677b2534d78814a3863dc8d9a5918
Enocp <pierre.enoc@gmail.com>
2020-04-27 12:12:06 -0300

    criar tela de login

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-class Card extends React.Component{
-  render(){
-    return(
-        <div className="card md-3">
-          <div className="card-header">{this.props.title}</div>
-         <div className="card-body">
-           {this.props.children}
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card md-3">
+        <h3 className="card-header">{this.props.title}</h3>
+        <div className="card-body">
+          {this.props.children}
 
-            </div>
-          </div>
-        
+        </div>
+      </div>
+
 
     )
   }
