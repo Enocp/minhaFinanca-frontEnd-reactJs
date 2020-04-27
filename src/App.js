a5cebe544c2d2a6ee95697f315c4f0f82de3cb45
Enocp <pierre.enoc@gmail.com>
2020-04-27 17:14:09 -0300

    cadastar usuario

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css'
 import Login from './views/login'
 import './custom.css'
+import CadastroUsuario from './views/cadastroUsuario'
 //import logo from './logo.svg';
 //import './App.css';
 
@@ -12,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Login />
+        <CadastroUsuario />
       </div>
     )
   }
