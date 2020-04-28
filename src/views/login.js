7957795c9e460eba837b38b7703518df753c5ec5
Enocp <pierre.enoc@gmail.com>
2020-04-28 14:45:12 -0300

    manupilando dados vindo da nossa api

--- src/views/login.js
+++ src/views/login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import Axios from 'axios'
 
 class Login extends React.Component {
@@ -14,60 +14,63 @@ class Login extends React.Component {
 
   entrar = () => {
     Axios
-    .post('http://localhost:8080/api/usuarios/autenticar', {
+      .post('http://localhost:8080/api/usuarios/autenticar', {
         email: this.state.email,
         senha: this.state.senha
-    }).then( response => {
-      console.log(response)
-    }).catch( erro => {
-      console.log(erro.response)
-    })
+      }).then(response => {
+        this.props.history.push('/home')
+      }).catch(erro => {
+        this.setState({ mensagemErro: erro.response.data })
+      })
   }
 
-    prepareCadastrar = () => {
-      this.props.history.push('/cadastro-usuarios')
-    }
+  prepareCadastrar = () => {
+    this.props.history.push('/cadastro-usuarios')
+  }
 
   render() {
     return (
 
-      
-        <div className="row">
-          <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
-            <div className="bs-docs-section">
-              <Card title="Login">
-                <div className="row">
-                  <div className="col-lg-12">
-                    <div className="bs-component">
-                      <fieldset>
-                        <FormGroup label="Email: *" htmlFor="exempleInputEmail">
-                          <input type="email" 
-                          value={this.state.email} 
-                          onChange={e => this.setState({ email: e.target.value })} 
-                          className="form-control" 
-                          id="exampleInputEmail1" aria-describedby="emailHelp" 
+
+      <div className="row">
+        <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+          <div className="bs-docs-section">
+            <Card title="Login">
+              <div className="row">
+                <span>{this.state.mensagemErro}</span>
+              </div>
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="bs-component">
+                    <fieldset>
+                      <FormGroup label="Email: *" htmlFor="exempleInputEmail">
+                        <input type="email"
+                          value={this.state.email}
+                          onChange={e => this.setState({ email: e.target.value })}
+                          className="form-control"
+                          id="exampleInputEmail1" aria-describedby="emailHelp"
                           placeholder="Digite o Email" />
-                        </FormGroup>
+                      </FormGroup>
 
-                        <FormGroup label="Senha:*" htmlFor="exampleInputPassword1">
-                          <input type="password"
-                            value={this.state.senha}
-                            onChange={e => this.setState({ senha: e.target.value })}
-                            className="form-control"
-                            id="exampleInputPassword1" placeholder="Password" />
+                      <FormGroup label="Senha:*" htmlFor="exampleInputPassword1">
+                        <input type="password"
+                          value={this.state.senha}
+                          onChange={e => this.setState({ senha: e.target.value })}
+                          className="form-control"
+                          id="exampleInputPassword1" placeholder="Password" />
 
-                        </FormGroup>
-                        <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                        <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
-                      </fieldset>
-                    </div>
-
+                      </FormGroup>
+                      <button onClick={this.entrar} className="btn btn-success">Entrar</button>
+                      <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
+                    </fieldset>
                   </div>
+
                 </div>
-              </Card>
-            </div>
+              </div>
+            </Card>
           </div>
         </div>
+      </div>
     )
   }
 
